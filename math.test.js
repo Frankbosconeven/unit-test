659dd115bc6b25b32c10ca593da04e90c99dbc3c
Seth Abbey <frankbosconeven@gmail.com>
2022-09-19 14:26:22 +0000

    first commit

---
+++ math.test.js
@@ -0,0 +1,31 @@
+const {divide, multiply, subtract, sum} = require("./math");
+
+describe("Math", () => {
+    it("Should sum 2 + 2 toBe 4", () => {
+        const result = sum(2,2);
+        expect(result).toBe(4);
+        expect(result).toBeGreaterThan(3)
+    });
+})
+
+it("Should multiply 3 * 3 to result to 9 ", () => {
+    const result = multiply(3,3);
+    expect(result).toBe(9)
+});
+it("Should divide 4 / 2 to result to 2 ", () => {
+    const result = divide(4,2);
+    expect(result).toBe(2)
+});
+it("Should subtract 10 - 5 to result to 5 ", () => {
+    const result = subtract(10, 5);
+    expect(result).toBe(5)
+});
+it("Should be falsy", () => {
+    expect(undefined).toBeFalsy();
+});
+
+describe("Array", () => {
+    let days = ["Monday", "Tuesday", "Wednesday"]
+    expect(days).toContain("Tuesday");
+    expect(days).toContain("Tuesday");
+})
